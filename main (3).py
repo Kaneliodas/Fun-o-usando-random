763dbd3965792406d8d0e7f911227fcd680c205c
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2025-10-17 18:18:10 -0300

    Add conditional statements for grading output

--- main (3).py
+++ main (3).py
@@ -2,4 +2,11 @@ import random
 def media(n1,n2,n3):
     return (n1+n2+n3)/3 
 a = media(random.randint(1,10),random.randint(1,10),random.randint(1,10))
-print(a)
+if a >= 7 and a < 10:
+   print(f" Aprovado, sua nota foi: {a}")
+elif a < 7:
+    print(f"Reprovado, sua nota foi: {a}")
+else:
+    print(f"Aprovado com distinção")
+    
+    
